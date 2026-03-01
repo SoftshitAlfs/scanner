f372e3fa652ff874f1bec3bb78c96d5720b0aa1f
SoftshitAlfs <sftalfie1@gmail.com>
2026-03-01 09:27:07 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,13 +4,15 @@ import asyncio
 import aiohttp
 import os
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 URL_REGEX = r"(https?://[^\s]+)"
+VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY')
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+SCANNER_LOGS_CHANNEL_IDS = [1477328726214578371, 1410480747604742154]
 
 async def scan_with_virustotal(url_to_scan, vt_api_key):
     headers = {
@@ -56,14 +58,17 @@ async def on_message(message):
     all_urls_to_scan = urls + attachment_urls
 
     if all_urls_to_scan:
-        log_channel = client.get_channel(SCANNER_LOGS_CHANNEL_ID)
+        status_messages = []
         
-        if not log_channel:
-            print("Log channel not found. Check your SCANNER_LOGS_CHANNEL_ID.")
+        for channel_id in SCANNER_LOGS_CHANNEL_IDS:
+            log_channel = client.get_channel(channel_id)
+            if log_channel:
+                msg = await log_channel.send(f"[System] Initializing scan for a message from {message.author.name} in {message.channel.name}...")
+                status_messages.append(msg)
+
+        if not status_messages:
             return
 
-        status_msg = await log_channel.send(f"**[System]** Initializing scan for a message from {message.author.name} in {message.channel.name}...")
-        
         async def run_animation():
             frames = [
                 f"[>.........] Starting scan for {message.author.name}...",
@@ -80,7 +85,8 @@ async def on_message(message):
             try:
                 while True:
                     for frame in frames:
-                        await status_msg.edit(content=frame)
+                        for msg in status_messages:
+                            await msg.edit(content=frame)
                         await asyncio.sleep(0.6)
             except asyncio.CancelledError:
                 pass
@@ -88,41 +94,30 @@ async def on_message(message):
         animation_task = asyncio.create_task(run_animation())
 
         is_malicious = False
-        
         for url in all_urls_to_scan:
             stats = await scan_with_virustotal(url, VIRUSTOTAL_API_KEY)
-            
             if stats:
                 if stats.get('malicious', 0) > 0 or stats.get('suspicious', 0) > 0:
                     is_malicious = True
                     break 
 
         animation_task.cancel()
-        
         scanned_links_text = "\n".join(all_urls_to_scan)
         
-        if is_malicious:
-            embed = discord.Embed(title="Threat Detected", color=discord.Color.red())
-            embed.add_field(name="User", value=message.author.mention, inline=True)
-            embed.add_field(name="Channel", value=message.channel.mention, inline=True)
-            embed.add_field(name="Message Link", value=f"[Jump to Message]({message.jump_url})", inline=False)
-            embed.add_field(name="Scanned Content", value=scanned_links_text, inline=False)
-            
-            await status_msg.edit(content="**[ALERT]** Malicious payload detected.", embed=embed)
-                
-        else:
-            embed = discord.Embed(title="Safe Content Scanned", color=discord.Color.green())
-            embed.add_field(name="User", value=message.author.mention, inline=True)
-            embed.add_field(name="Channel", value=message.channel.mention, inline=True)
-            embed.add_field(name="Message Link", value=f"[Jump to Message]({message.jump_url})", inline=False)
-            embed.add_field(name="Scanned Content", value=scanned_links_text, inline=False)
-            
-            await status_msg.edit(content="**[CLEAR]** Content verified as secure.", embed=embed)
-
-
-VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY')
-
-DISCORD_BOT_TOKEN = os.getenv ('DISCORD_BOT_TOKEN')
-SCANNER_LOGS_CHANNEL_ID = 1477328726214578371
+        for msg in status_messages:
+            if is_malicious:
+                embed = discord.Embed(title="Threat Detected", color=discord.Color.red())
+                embed.add_field(name="User", value=message.author.mention, inline=True)
+                embed.add_field(name="Channel", value=message.channel.mention, inline=True)
+                embed.add_field(name="Message Link", value=f"[Jump to Message]({message.jump_url})", inline=False)
+                embed.add_field(name="Scanned Content", value=scanned_links_text, inline=False)
+                await msg.edit(content="[ALERT] Malicious payload detected.", embed=embed)
+            else:
+                embed = discord.Embed(title="Safe Content Scanned", color=discord.Color.green())
+                embed.add_field(name="User", value=message.author.mention, inline=True)
+                embed.add_field(name="Channel", value=message.channel.mention, inline=True)
+                embed.add_field(name="Message Link", value=f"[Jump to Message]({message.jump_url})", inline=False)
+                embed.add_field(name="Scanned Content", value=scanned_links_text, inline=False)
+                await msg.edit(content="[CLEAR] Content verified as secure.", embed=embed)
 
 client.run(DISCORD_BOT_TOKEN)
